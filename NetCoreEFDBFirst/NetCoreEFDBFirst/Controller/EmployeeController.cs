dbd7195572b469f531b17b3233ae85b936fabe10
chaitali-narkhede <chairali.narkhede1991@gmail.com>
2021-08-04 15:18:52 +0530

    check-in

---
+++ NetCoreEFDBFirst/NetCoreEFDBFirst/Controller/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreEFDBFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreEFDBFirst.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmployeeDBContext employeeDBContext = null;
+        public EmployeeController(EmployeeDBContext _employeeDBContext)
+        {
+            this.employeeDBContext = _employeeDBContext;
+        }
+    }
+}

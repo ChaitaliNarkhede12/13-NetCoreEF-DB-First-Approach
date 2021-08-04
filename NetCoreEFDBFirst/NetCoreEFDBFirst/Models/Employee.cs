489c3c0a85a951d01a471af4eb6cc377a2a1ccb8
chaitali-narkhede <chairali.narkhede1991@gmail.com>
2021-08-04 15:11:39 +0530

    First check-in

---
+++ NetCoreEFDBFirst/NetCoreEFDBFirst/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace NetCoreEFDBFirst.Models
+{
+    public partial class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpEmail { get; set; }
+        public string EmpAddress { get; set; }
+    }
+}
---
+++ NetCoreEFDBFirst/NetCoreEFDBFirst/Models/EmployeeDBContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace NetCoreEFDBFirst.Models
+{
+    public partial class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext()
+        {
+        }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=DESKTOP-3DF0FM6\\SQLEXPRESS;Database=EmployeeDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasKey(e => e.DeptId)
+                    .HasName("PK__Departme__014881AE1B2F83E7");
+
+                entity.ToTable("Department");
+
+                entity.Property(e => e.DeptName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.EmpId)
+                    .HasName("PK__Employee__AF2DBB99D7501E63");
+
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmpAddress)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpEmail)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
